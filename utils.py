71160744c1620320e5d38df2639ef2e3d134d457
Luoqiu76 <826340087@qq.com>
2025-01-15 06:46:36 +0000

    update

--- utils.py
+++ utils.py
@@ -120,8 +120,8 @@ async def execute_with_retry_async(func, max_retries=5, enable_wait = True, *arg
             except Exception as e:
                 retries += 1
                 errors.append((e, traceback.format_exc()))
-                # print(f"Error: {e}")
-                # print (f"Traceback: {traceback.format_exc()}")
+                print(f"Error: {e}")
+                print (f"Traceback: {traceback.format_exc()}")
                 if enable_wait:
                     await asyncio.sleep(get_wait_time(retries))
                 if retries >= max_retries:
